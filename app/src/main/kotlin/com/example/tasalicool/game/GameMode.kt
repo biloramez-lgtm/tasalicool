e5a6a9e60156b588ec41f3fbd813b3635ef5ed34
Mister Milian <biloramez@gmail.com>
2026-02-12 17:25:55 +0200

    Update GameMode.kt

--- app/src/main/kotlin/com/example/tasalicool/game/GameMode.kt
+++ app/src/main/kotlin/com/example/tasalicool/game/GameMode.kt
@@ -1,1 +1,10 @@
-.
+package com.example.tasalicool.game
+
+enum class GameMode {
+
+    SINGLE_PLAYER,      // لاعب ضد 3 AI
+
+    LOCAL_MULTIPLAYER,  // لاعبين حقيقيين + 2 AI
+
+    WIFI_MULTIPLAYER    // 4 لاعبين عبر الشبكة
+}
