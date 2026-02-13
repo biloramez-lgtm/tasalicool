3cd428f505d18e2b82b397e7097503d234c1b2d1
Mister Milian <biloramez@gmail.com>
2026-02-13 16:31:09 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.13"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     packaging {
@@ -62,15 +62,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
 
-    // 🔥 Compose BOM (حديث ومستقر)
+    // 🔥 Compose BOM
     implementation(platform("androidx.compose:compose-bom:2024.04.01"))
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")   // ✅ أضفناها
     implementation("androidx.compose.material3:material3")
 
-    // Icons (يدعم AutoMirrored)
+    // Icons
     implementation("androidx.compose.material:material-icons-extended")
 
     // ViewModel
